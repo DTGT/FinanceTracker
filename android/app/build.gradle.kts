dfbb36688324e60672a9014cb9f88e0f3de5ba74
Dee Fernandez <daundeef@gmail.com>
2025-11-24 08:55:46 +0800

    First apk release

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -22,15 +22,14 @@ android {
         jvmTarget = JavaVersion.VERSION_17.toString()
     }
 
-    defaultConfig {
-        // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
+     defaultConfig {
         applicationId = "com.example.financial_tracker"
-        // You can update the following values to match your application needs.
-        // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
-        targetSdk = flutter.targetSdkVersion
-        versionCode = flutter.versionCode
-        versionName = flutter.versionName
+        targetSdk = 33
+        versionCode = 1
+        versionName = "1.0"
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
 
     buildTypes {
